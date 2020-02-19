06e16c4fc5120a8c4ef621ed6bce08de8eb1d67e
Alexandr <alex.black@inbox.ru>
2020-02-19 23:37:57 +0200

    First commit

---
+++ javascript.js
@@ -0,0 +1,39 @@
+'use strict';
+
+//alert ("Simple alert");
+//[0,5].push();
+
+
+//var Varic1 = 'abs';
+//alert(Varic1);
+/*{
+    let Varic2 = 2;
+    const Varic3 = 3;
+    console.log(Varic3);
+    console.log(Varic2);
+    console.log(Varic1);
+}
+*/
+/*let Sym = Symbol('hghg');
+console.log(4*0);
+console.log('text'/0);
+console.log('Jopa' + 9);
+console.log(Sym);
+
+let fio = {
+    name: 'Alex',
+    age: 19,
+    isdriver: true
+};
+
+console.log(fio.age, fio.isdriver);
+
+let ans = prompt('Ty durak', 'Y/N');
+console.log(ans);
+*/
+//console.log(typeof(+'4'));
+let incr =100,
+    decr =100;
+//incr++;
+//decr--;
+console.log(++incr,--decr);
